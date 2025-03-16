ef89bf302c2cebdb2cb09c97fec810fde3ccd17f
Melone155 <101599412+Melone155@users.noreply.github.com>
2025-03-17 00:36:24 +0100

    Uploade SQL-Book-Preview.sql

---
+++ SQL_Entrys/SQL-Book-Preview.sql
@@ -0,0 +1,73 @@
+USE bookmanager;
+
+INSERT INTO authors (Name) VALUES ('Anna Todd');
+INSERT INTO authors (Name) VALUES ('Marc Raabe');
+INSERT INTO authors (Name) VALUES ('Michael Tsokos');
+INSERT INTO authors (Name) VALUES ('Michael Kobr');
+INSERT INTO authors (Name) VALUES ('Holly Renee');
+INSERT INTO authors (Name) VALUES ('Sebastian Fitzek');
+INSERT INTO authors (Name) VALUES ('Stephanie Garber');
+INSERT INTO authors (Name) VALUES ('Tatsuya Endo');
+
+INSERT INTO books VALUES ('9783453491168','After passion','2015', '1');
+INSERT INTO books VALUES ('9783453491175','After truth','2015', '1');
+INSERT INTO books VALUES ('9783453418837','After forever','2015', '1');
+INSERT INTO books VALUES ('9783453491182','After love','2015', '1');
+
+INSERT INTO books VALUES ('9783843729154','Der Morgen','2023', '2');
+INSERT INTO books VALUES ('9783548290997','Schlüssel 17','2019', '2');
+INSERT INTO books VALUES ('9783548285917','Schnitt','2014', '2');
+
+INSERT INTO books VALUES ('9783426446256','Der zweite Verdächtige','2025', '3');
+INSERT INTO books VALUES ('9783426527559','Die siebte Zeugin','2021', '3');
+INSERT INTO books VALUES ('9783426446232','Der 1. Patient','2024', '3');
+INSERT INTO books VALUES ('9783426528457','Die letzte Lügnerin','2023', '3');
+INSERT INTO books VALUES ('9783426528440','Der dreizehnte Mann','2022', '3');
+INSERT INTO books VALUES ('9783426528723','Mit kaltem Kalkül','2024', '3');
+INSERT INTO books VALUES ('9783426528709','Mit kalter Präzision','2023', '3');
+INSERT INTO books VALUES ('9783426461945','Die siebte Zeugin','2021', '3');
+INSERT INTO books VALUES ('9783426528686','Zerteilt','2022', '3');
+INSERT INTO books VALUES ('9783426524398','Abgeschlagen','2021', '3');
+INSERT INTO books VALUES ('9783426528716','Mit kalter Präzision','2024', '3');
+INSERT INTO books VALUES ('9783426659984','Dem Tod auf der Spur','2024', '3');
+INSERT INTO books VALUES ('9783426525500','Zerrissen','2023', '3');
+INSERT INTO books VALUES ('9783426510919','Abgeschnitten','2013', '3');
+
+INSERT INTO books VALUES ('9783550201479','Lückenbüßer','2024', '4');
+INSERT INTO books VALUES ('9783548067568','Affenhitze','2023', '4');
+INSERT INTO books VALUES ('9783442495887','Sonne über Gudhjem','2024', '4');
+INSERT INTO books VALUES ('9783442316908','Nebel über Rønne','2024', '4');
+INSERT INTO books VALUES ('9783548064918','Funkenmord','2021', '4');
+INSERT INTO books VALUES ('9783550201448','Die Unverbesserlichen - Der große Coup des Monsieur Lipaire','2022', '4');
+INSERT INTO books VALUES ('9783426511831','Herzblut','2014', '4');
+
+INSERT INTO books VALUES ('9783551586025','The Veiled Kingdom','2025', '5');
+INSERT INTO books VALUES ('9783551586032','The Hunted Heir','2025', '5');
+INSERT INTO books VALUES ('9783551585530','The Songbird and the Heart of Stone','2025', '5');
+
+INSERT INTO books VALUES ('9783426281741','Das Kalendermädchen','2024', '6');
+INSERT INTO books VALUES ('9783426519493','Die Einladung','2024', '6');
+INSERT INTO books VALUES ('9783426519486','Mimik','2023', '6');
+INSERT INTO books VALUES ('9783426561195','Horror-Date','2025', '6');
+INSERT INTO books VALUES ('9783426284131','Elternabend','2023', '6');
+INSERT INTO books VALUES ('9783426519448','Der Insasse','2020', '6');
+INSERT INTO books VALUES ('9783426519462','Der Heimweg','2022', '6');
+
+INSERT INTO books VALUES ('9783492708210','Caraval','2023', '7');
+INSERT INTO books VALUES ('9783492708234','Finale','2023', '7');
+INSERT INTO books VALUES ('9783492708227','Legendary','2023', '7');
+INSERT INTO books VALUES ('9783492708241','Spectacular','2024', '7');
+
+INSERT INTO books VALUES ('9782889513505','Spy x Family - Band 1','2020', '8');
+INSERT INTO books VALUES ('9782889513512','Spy x Family - Band 2','2020', '8');
+INSERT INTO books VALUES ('9782889513529','Spy x Family - Band 3','2021', '8');
+INSERT INTO books VALUES ('9782889513536','Spy x Family - Band 4','2021', '8');
+INSERT INTO books VALUES ('9782889513543','Spy x Family - Band 5','2021', '8');
+INSERT INTO books VALUES ('9782889513550','Spy x Family - Band 6','2021', '8');
+INSERT INTO books VALUES ('9782889513567','Spy x Family - Band 7','2021', '8');
+INSERT INTO books VALUES ('9782889513574','Spy x Family - Band 8','2022', '8');
+INSERT INTO books VALUES ('9782889513581','Spy x Family - Band 9','2022', '8');
+INSERT INTO books VALUES ('9782889513598','Spy x Family - Band 10','2023', '8');
+INSERT INTO books VALUES ('9782832470107','Spy x Family - Band 11','2023', '8');
+INSERT INTO books VALUES ('9782832470114','Spy x Family - Band 12','2024', '8');
+INSERT INTO books VALUES ('9782832470121','Spy x Family - Band 13','2024', '8');
